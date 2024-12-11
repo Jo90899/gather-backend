c41004ccf888292ed73009760ac89c4a160eadb3
Jo90899 <jabdelmalek@hmc.edu>
2024-12-11 15:51:13 -0800

    added domain name

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://5.78.125.190'],
+  origin: ['http://localhost:3000', 'http://5.78.125.190', 'https://gather-maps.com'],
   credentials: true
 }));
 
